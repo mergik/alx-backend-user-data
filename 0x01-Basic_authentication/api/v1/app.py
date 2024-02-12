26aaf047f68f8fc68e365bb66ebb3056e675614f
mergik <gmerlix@yahoo.com>
2024-02-12 13:06:56 +0300

    Implement a Basic Authentication on a simple API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,16 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth_type = os.getenv("AUTH_TYPE", "basic")
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -19,6 +29,34 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.before_request
+def before_request():
+    """Filters requests to error handlers
+    """
+    path_list = ['/api/v1/status/',
+                 '/api/v1/unauthorized',
+                 '/api/v1/forbidden']
+    if auth and auth.require_auth(request.path, path_list):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
